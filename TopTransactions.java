e52d515ad3f198a6770c268b852bac075b7ab5b8
Jenny Baran <jenny.baran@gmail.com>
2018-04-01 13:33:04 -0400

    got data into arraylist & back out again

--- TopTransactions.java
+++ TopTransactions.java
@@ -5,13 +5,20 @@ public class TopTransactions {
 
     public static void main(String[] args) throws Exception {
         File input = new File("transactions.txt"); // just gotta make this come from cmdline
-        ArrayList<String> inputList = new ArrayList<String>();
+        ArrayList<String[]> inputList = new ArrayList<String[]>();
         Scanner scanner = new Scanner(input);
         
         while (scanner.hasNextLine() ) {
-            inputList.add(scanner.nextLine() + "\n");            
+            String s = scanner.nextLine();
+            String[] temp = s.split(" +");
+            inputList.add(temp);
         }
-        System.out.println(inputList);
-    
+        
+        for( int i = 0; i < inputList.size(); i++ ) {
+            String[] temp = inputList.get(i);
+            for( int j = 0; j < temp.length; j++ ) {
+                System.out.println( temp[j] );
+            }
+        }
     }
 }
